785fe5d3f5ec3721a439320dcf2e589fe3a6312e
gurmant <96808510+guruduttabhau@users.noreply.github.com>
2021-12-29 06:07:19 +0000

    data

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas
+data={
+
+  'series_1':[1,3,4,5,7],
+  'series2':[10,30,40,100,250]
+}
+df=pandas.DataFrame(data)
 st.title("First app")
 st.subheader("automation")
 st.write('''This is our first app
 ''')
+st.write(df)
